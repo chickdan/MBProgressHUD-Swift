25a690d36c9320427c151fa1a8dceb92a4caeb9a
Daniel Chick <daniel.chick@homebase.ai>
2023-06-01 15:15:34 -0500

    Move Background view to its own file

---
+++ MBBackgroundView.swift
@@ -0,0 +1,69 @@
+//
+//  MBBackgroundView.swift
+//  MBProgressHUD
+//
+//  Created by Daniel Chick on 6/1/23.
+//  Copyright © 2023 Matej Bukovinski. All rights reserved.
+//
+
+import UIKit
+
+class MBBackgroundView: UIView {
+
+    /**
+     * The background color or the blur tint color.
+     */
+    var color: UIColor {
+        didSet {
+            self.backgroundColor = self.color
+            if self.color == .clear {
+                effectView.removeFromSuperview()
+            } else {
+                addSubview(effectView)
+            }
+        }
+    }
+
+    private var effectView: UIVisualEffectView
+
+    // MARK: - -  Lifecycle
+    override init(frame: CGRect) {
+        if kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_8_0 {
+            color = UIColor(white: 0.8, alpha: 0.6)
+        } else {
+            color = UIColor(white: 0.95, alpha: 0.6)
+        }
+        effectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+
+        super.init(frame: frame)
+
+        clipsToBounds = true
+        addSubview(effectView)
+        effectView.frame = bounds
+        effectView.autoresizingMask = [.flexibleHeight, .flexibleWidth ]
+        backgroundColor = color
+        layer.allowsGroupOpacity = false
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        if kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_8_0 {
+            color = UIColor(white: 0.8, alpha: 0.6)
+        } else {
+            color = UIColor(white: 0.95, alpha: 0.6)
+        }
+        effectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+
+        super.init(coder: aDecoder)
+
+        clipsToBounds = true
+        addSubview(effectView)
+        effectView.frame = bounds
+        effectView.autoresizingMask = [.flexibleHeight, .flexibleWidth ]
+        backgroundColor = color
+        layer.allowsGroupOpacity = false
+    }
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize.zero
+    }
+}
