78529024b2c5a787b3df41b907276cb0c9665a1c
Daniel Chick <daniel.chick@homebase.ai>
2023-10-18 23:10:18 -0500

    Default style to grouped

--- Demo/HudDemoSwift/HudDemoSwift/ViewController.swift
+++ Demo/HudDemoSwift/HudDemoSwift/ViewController.swift
@@ -57,12 +57,20 @@ class ViewController: UITableViewController {
     var canceled: Bool = false
     
     let examples: [[ProgressMode]] = [
-        [.special],
+//        [.special],
         [.indeterminate, .indeterminateLabel, .indeterminateDetails],
         [.determinate, .determinateAnnular, .determinateBar],
         [.text, .customView, .actionButton, .modeSwitching],
         [.onWindow, .networking, .determinateProgress, .dimBackground, .colored]
     ]
+    
+    init() {
+        super.init(style: .grouped)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
